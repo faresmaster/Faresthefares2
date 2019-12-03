536dd9dadd565d9b712e47d35bb64230ec7d09e5
Fares <fares.elsherbiniy.1@gmail.com>
2019-12-03 19:59:58 +0200

    Update app.js

---
+++ M01S06/ex01/app.js
@@ -0,0 +1,50 @@
+let car = {
+  make: 'Audi',
+  color: 'black',
+  wheels: 4,
+  speed: 50,
+  topSpeed: 160,
+  reverseSpeed: -50,
+  isTrunkOpen: false,
+  areLightsOn:false,
+  turnLightsOn: function() {
+    this. areLightsOn = true;
+  },
+  turnLightsOff: function() {
+    this. areLightsOff = false;
+  },
+  flashlights: function(){
+    this.turnLightsOff();
+    setTimeout(function() {
+    this.turnLightsOff()
+    } , 800);
+  } ,
+
+  openTrunk: function() {
+    this.isTrunkOpen = true;
+  } , 
+  closeTrunk: function() {
+    this.isTrunkOpen = false;
+  } , 
+  accelerate: function() {
+    this.speed++;
+  },
+  decelerate: function() {
+    this.speed--;
+  }, 
+  stop: function() {
+    this.speed=0;
+  },
+  setSpeed: function(speed) {
+    if (speed > this.speed) {
+    this.speed = this.topSpeed;
+    return;
+  }
+
+console.log(`The car is an ${car.make} and moves with a speed of ${car.speed} km/h`);
+car.decelerate();
+car.decelerate();
+car.decelerate();
+car.decelerate();
+car.decelerate();
+console.log(`the new speed is ${car.speed}`)
