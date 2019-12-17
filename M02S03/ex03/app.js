83b67ec3cc07e6374963ab53fe10f40b9e4ddd7f
Fares <fares.elsherbiniy.1@gmail.com>
2019-12-17 19:59:38 +0200

    12w3er5t6y7uytgfrdes

--- M02S03/ex03/app.js
+++ M02S03/ex03/app.js
@@ -1,1 +1,15 @@
+let stage = document.querySelector('.stage');
+let message = document.querySelector('.message');
+let countElement = document.querySelector('.count')
+let count = 0;
 
+stage.addEventListener('mouseover', () => {
+  message.innerText = 'mouse is on scene';
+  count = count + 1;
+
+});
+
+stage.addEventListener('mouseout', () => {
+  message.innerText = 'mouse is out of the scene';
+  countElement.innerText = `mouse was on scene ${count}times`;
+});
